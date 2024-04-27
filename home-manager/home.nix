312f2f925a1ececabc5981446904d89384dc6664
Bogus Kladik <boguskladik@gmail.com>
2024-04-27 18:36:12 +0300

    simple changes

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -179,6 +179,7 @@ in rec {
     unstable.gcc
 
     clips
+    swiProlog
 
     nil
     nixfmt
@@ -203,7 +204,7 @@ in rec {
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    ".background".source = ../resources/wp1.png;
+    ".background".source = ../resources/nix-Wallpaper.png;
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
